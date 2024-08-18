1fb4b3ced6d82064852dcf31b089c2db88f78db7
ingfaraya <162543145+ingfaraya@users.noreply.github.com>
2024-08-18 03:07:51 -0400

    taller - JCompose

---
+++ Taller_V4/app/src/main/java/com/example/taller_v4/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.taller_v4
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = {
+                val navigate = Intent(this@MainActivity, OtraActividad::class.java)
+                    startActivity(navigate)
+                }) { Text(text = "Navegar a otra actividad") }
+            }
+        }
+    }
+}
+
+
+
---
+++ Taller_V4/app/src/main/java/com/example/taller_v4/OtraActividad.kt
@@ -0,0 +1,28 @@
+package com.example.taller_v4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class OtraActividad : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hola soy otra actividad")
+            }
+        }
+    }
+}
